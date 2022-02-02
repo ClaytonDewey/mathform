ed51c0cc7b253986bee7878986bc9f5cdec7b2da
ClaytonDewey <clay@dryadmedia.com>
2022-02-02 08:53:49 -0600

    Import FormGroud and FormControl

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent implements OnInit {
+    mathForm = new FormGroup({
+        a: new FormControl(''),
+        b: new FormControl(''),
+        answer: new FormControl('')
+    })
 
   constructor() { }
 
